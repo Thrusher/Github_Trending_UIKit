640b098c7acca8732ea2cdab1257e8e229da003b
Patryk Drozd <drozd.patryk89@gmail.com>
2020-09-20 17:23:06 +0200

    Initial Commit

---
+++ Github_Trending_UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Github_Trending_UIKit
+//
+//  Created by Patryk Drozd on 20/09/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
